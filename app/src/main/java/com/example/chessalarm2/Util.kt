3e6573765afd5488f86ed9584f0b683065b8dabc
Alexander Berntsson <alexande.berntsson@gmail.com>
2021-11-06 14:41:09 +0100

    created parser for UCI format (move sequence)

--- app/src/main/java/com/example/chessalarm2/Util.kt
+++ app/src/main/java/com/example/chessalarm2/Util.kt
@@ -2,6 +2,7 @@ package com.example.chessalarm2
 
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.chessalarm2.chessproblemalarm.Coordinate
 
 class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
 
@@ -23,4 +24,15 @@ fun daysToString(days: List<Int>) : String {
         }
     }
     return res
+}
+
+fun parse_UCI(UCI: String): List<Pair<Coordinate, Coordinate>> {
+    val moves_string = UCI.split(" ")
+    val moves = mutableListOf<Pair<Coordinate, Coordinate>>()
+    for (move_string in moves_string) {
+        val src = Coordinate(move_string[0].code-'a'.code, 8-move_string[1].digitToInt())
+        val dst = Coordinate(move_string[2].code-'a'.code, 8-move_string[3].digitToInt())
+        moves.add(Pair(src,dst))
+    }
+    return moves
 }
