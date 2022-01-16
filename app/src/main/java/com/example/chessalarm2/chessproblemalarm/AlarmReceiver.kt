a7cd3b2043634b06e50794b56bd3f11e6a5e79df
Alexander Berntsson <alexande.berntsson@gmail.com>
2022-01-16 18:23:41 +0100

    changed audio type to alarm

--- app/src/main/java/com/example/chessalarm2/chessproblemalarm/AlarmReceiver.kt
+++ app/src/main/java/com/example/chessalarm2/chessproblemalarm/AlarmReceiver.kt
@@ -6,6 +6,7 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Build
 import android.util.Log
@@ -37,6 +38,11 @@ class AlarmReceiver : BroadcastReceiver() {
         GlobalScope.launch {
             val alarm = database.get(alarmId)!!
             mediaPlayer = MediaPlayer()
+            mediaPlayer!!.setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+            )
             Log.d("onReceive", alarm.audioId.toString())
             playAudioFromId(context, mediaPlayer!!, alarm.audioId)
         }
