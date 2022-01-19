ee70b22a8f2ce2e4b2cd8eca3a8557852de11d4b
Alexander Berntsson <alexande.berntsson@gmail.com>
2022-01-20 00:03:09 +0100

    scheduling at specific days and time work now

--- app/src/main/java/com/example/chessalarm2/Scheduler.kt
+++ app/src/main/java/com/example/chessalarm2/Scheduler.kt
@@ -4,7 +4,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.SystemClock
 import android.util.Log
 import com.example.chessalarm2.chessproblemalarm.AlarmReceiver
 import com.example.chessalarm2.database.alarms.Alarm
@@ -17,37 +16,54 @@ class Scheduler(val context: Context) {
     fun disableAlarm(alarm: Alarm) {
         val date = Date(alarm.time)
         Log.d("disableAlarm", date.toString())
-        val alarmIntent = getAlarmIntent(alarm)
-        alarmManager.cancel(alarmIntent)
+        for (day in alarm.days) {
+            val alarmIntent = getAlarmIntent(alarm, day)
+            alarmManager.cancel(alarmIntent)
+
+        }
     }
 
     fun enableAlarm(alarm: Alarm) {
-        val date = Date(alarm.time)
-        val c = Calendar.getInstance()
-        c.time = date
-        Log.d("enableAlarm", date.toString())
-        val calendar: Calendar = Calendar.getInstance().apply {
-            timeInMillis = System.currentTimeMillis()
-            set(Calendar.HOUR_OF_DAY, c.get(Calendar.HOUR_OF_DAY))
-            set(Calendar.MINUTE, c.get(Calendar.MINUTE))
+        for (day in alarm.days) {
+            val date = Date(alarm.time)
+            val c = Calendar.getInstance()
+            c.time = date
+            val calendar: Calendar = Calendar.getInstance().apply {
+                timeInMillis = System.currentTimeMillis()
+                if (timeInMillis < System.currentTimeMillis()) {
+                    add(Calendar.DAY_OF_YEAR,7)
+                }
+                set(Calendar.HOUR_OF_DAY, c.get(Calendar.HOUR_OF_DAY))
+                set(Calendar.MINUTE, c.get(Calendar.MINUTE))
+                set(Calendar.DAY_OF_WEEK, day+2)
+            }
+            val alarmIntent = getAlarmIntent(alarm, day)
+            Log.d("scheduler", "set alarm at "+Date(calendar.timeInMillis).toString())
+            /*alarmManager.set(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 1 * 1000,
+                alarmIntent
+            )*/
+            alarmManager.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                AlarmManager.INTERVAL_DAY*7,
+                alarmIntent
+            )
         }
-        val alarmIntent = getAlarmIntent(alarm)
-        alarmManager.set(
-            AlarmManager.ELAPSED_REALTIME_WAKEUP,
-            SystemClock.elapsedRealtime() + 1 * 1000,
-            alarmIntent
-        )
-        //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, alarmIntent)
-
     }
 
-    private fun getAlarmIntent(alarm: Alarm) : PendingIntent {
+    private fun getAlarmIntent(alarm: Alarm, day: Int) : PendingIntent {
         //val application = context.getApplication<Application>()
         val alarmIntent = Intent(context, AlarmReceiver::class.java).let { intent ->
             intent.action = "com.example.alarmmanager"
             intent.putExtra("alarmId", alarm.alarmId)
-            PendingIntent.getBroadcast(context, alarm.alarmId.toInt(), intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            PendingIntent.getBroadcast(context, getIntentId(alarm.alarmId, day), intent, PendingIntent.FLAG_UPDATE_CURRENT)
         }
         return alarmIntent
+    }
+
+    private fun getIntentId(alarmId: Long, day: Int): Int {
+        return alarmId.toInt()+(1 shl 31-day)
     }
 }
