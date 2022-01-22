7021294cf9f2609ba8bc0dd9ac128b94d9a4fe8f
Alexander Berntsson <alexande.berntsson@gmail.com>
2022-01-22 15:52:16 +0100

    removed save button

--- app/src/main/java/com/example/chessalarm2/configure/ConfigureFragment.kt
+++ app/src/main/java/com/example/chessalarm2/configure/ConfigureFragment.kt
@@ -3,7 +3,9 @@ package com.example.chessalarm2.configure
 import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
@@ -31,18 +33,6 @@ class ConfigureFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_save -> {
-            Log.d("SaveButton", "pressed")
-            //configureViewModel.alarm.value!!.difficulty =
-            configureViewModel.saveAlarm()
-            true
-        }
-        else -> {
-            super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onPause() {
@@ -148,11 +138,6 @@ class ConfigureFragment : Fragment() {
                 binding.spinnerAudio.setSelection(i)
             }
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.options_menu, menu)
     }
 }
 
